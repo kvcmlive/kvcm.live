3c91e83d1500fbe4020e748563662fae16c9d8e2
Tibet Tornaci <oofdere@hey.com>
2023-05-21 00:54:26 -0700

    make show pages with missing data work a bit nicer

--- src/routes/(client)/shows/[id]/+page.svelte
+++ src/routes/(client)/shows/[id]/+page.svelte
@@ -11,22 +11,29 @@
 	<h1>{data.show.title}</h1>
 	<hr />
 	<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2">
-		<div class="card overflow-clip w-full md:w-96 col-span-1">
+		<div class="card overflow-clip w-full md:w-96 col-span-1 h-min">
 			<div class="aspect-square w-full bg-red-500">
-				<img
-					class="aspect-square w-full"
-					src={pb.files.getUrl(data.show, data.show.cover, { thumb: '256x256' })}
-					srcset="{pb.files.getUrl(data.show, data.show.cover, { thumb: '128x128' })} 128w,
+				{#if data.show.cover}
+					<img
+						class="aspect-square w-full"
+						src={pb.files.getUrl(data.show, data.show.cover, { thumb: '256x256' })}
+						srcset="{pb.files.getUrl(data.show, data.show.cover, { thumb: '128x128' })} 128w,
 							{pb.files.getUrl(data.show, data.show.cover, { thumb: '256x256' })} 256w,
 							{pb.files.getUrl(data.show, data.show.cover, { thumb: '512x512' })} 512w,
 							{pb.files.getUrl(data.show, data.show.cover, { thumb: '1024x1024' })} 1024w "
-					alt={data.show.title}
-					loading="lazy"
-				/>
+						alt={data.show.title}
+					/>
+				{:else}
+					<img class="aspect-square w-full" src="/placeholder.jpg" alt={data.show.title} />
+				{/if}
 			</div>
 			<div class="p-2">
-				<h3>Hosted by {data.show.hosts}</h3>
-				<h3>{data.show.status}</h3>
+				{#if data.show.hosts}
+					<h3>Hosted by {data.show.hosts}</h3>
+				{/if}
+				{#if data.show.status}
+					<h3>{data.show.status}</h3>
+				{/if}
 			</div>
 		</div>
 
