71a5718a177fe05a8ad1511ebfaba619f3cb1a3f
Tibet Tornaci <oofdere@hey.com>
2023-09-18 20:57:05 -0700

    improve staff ui

--- src/routes/(client)/about/staff/+page.svelte
+++ src/routes/(client)/about/staff/+page.svelte
@@ -8,18 +8,24 @@
 </script>
 
 <div class="container m-auto flex flex-col gap-4 py-4">
-	<h1 class="h1">Shows</h1>
+	<h1 class="h1">Staff</h1>
 
 	<hr />
 
 	<ul class="grid grid-cols-1 md:grid-cols-2 gap-2">
 		{#each data.staff as member}
-			<div>
-				<img src={pb.files.getUrl(member, member.avatar)} />
-				<div>{member.name}</div>
-				<div>{@html member.roles}</div>
-			</div>
+			<li class="card flex overflow-clip">
+				<Avatar
+					src={pb.files.getUrl(member, member.avatar)}
+					fallback="/static/placeholder.jpg"
+					width="w-48"
+					rounded="rounded-none"
+				/>
+				<div class="p-2 px-4">
+					<p class="h3">{member.name}</p>
+					<p>{@html member.roles}</p>
+				</div>
+			</li>
 		{/each}
 	</ul>
 </div>
-rge
